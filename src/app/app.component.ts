93cc8a76714844322e3078d9e5a5c549be384b34
tlkhanh <longkhanhta@gmail.com>
2023-11-05 23:22:04 +0700

    add trigger change detection

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,23 +1,25 @@
-import { Component } from '@angular/core';
+import { ApplicationRef, Component, ElementRef } from '@angular/core';
+import { highLight } from 'src/highLight';
 
 @Component({
   selector: 'app-root',
   template: `
+    <button (click)="app.tick()">Trigger change detection</button>
     <ul class="tree">
       <li>
+        {{ highLight() }}
         <span>App Component</span>
         <ul>
+          <li><app-header></app-header></li>
           <li><app-footer></app-footer></li>
-          <li>
-            <span>HeaderComponent</span>
-            <ul>
-              <li><span>QuickSearchComponent</span></li>
-              <li><span>ProfileComponent</span></li>
-            </ul>
-          </li>
         </ul>
       </li>
     </ul>
   `,
 })
-export class AppComponent {}
+export class AppComponent {
+  constructor(public app: ApplicationRef, private el: ElementRef) { }
+  highLight() {
+    highLight(this.el);
+  }
+}
